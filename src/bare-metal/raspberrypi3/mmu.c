724c8e22fa283b871be9894b7136141459b928ac
Tom Kostler <tomtomkost@gmail.com>
2026-01-31 15:55:08 +0100

    BFIX - Improved Space Isolation.

--- src/bare-metal/raspberrypi3/mmu.c
+++ src/bare-metal/raspberrypi3/mmu.c
@@ -60,30 +60,35 @@ void disable_mmu() {
 }
 
 
-void createPageTableEntries(uint32_t physical_adr, uint32_t virtual_adr, uint32_t length, bool cacheable, bool readOnly, bool executeNever) {
+void createPageTableEntries(uint32_t physical_adr, uint32_t virtual_adr, uint32_t length, bool cacheable, bool read_only, bool execute_never, bool no_access) {
     mmu_level1_section_t section;
-    section.value       = 0;
-    section.bits._zero  = 0;
-    section.bits._one   = 1;
-    section.bits.domain = 0;
+    section.value = 0;
 
-    if(cacheable) {
-        // Outer and Inner Write-Back, no Write-Allocate
-        section.bits.b = 1;
-        section.bits.c = 1;
+
+    // Only init page table entries if access is allowed for the partition
+    if(!no_access) {
+        section.bits._zero  = 0;
+        section.bits._one   = 1;
+        section.bits.domain = 0;
+
+        if(cacheable) {
+            // Outer and Inner Write-Back, no Write-Allocate
+            section.bits.b = 1;
+            section.bits.c = 1;
+        }
+
+        // Access Permission Bits
+        section.bits.ap1_0 = 3;                 // Access Permitted for User & Privileged
+        section.bits.ap2   = read_only ? 1 : 0; // Read/Write or ReadOnly Access
+
+        // Execute Never Bit (XN)
+        section.bits.xn = execute_never ? 1 : 0;
     }
 
-    // Access Permission Bits
-    section.bits.ap1_0 = 3;                // Access Permitted for User & Privileged
-    section.bits.ap2   = readOnly ? 1 : 0; // Read/Write or ReadOnly Access
-
-    // Execute Never Bit (XN)
-    section.bits.xn = executeNever ? 1 : 0;
 
     uint32_t physical_base = physical_adr >> 20;
-
-
     uint32_t start_section = virtual_adr >> 20;
+
 
     if(length == 0) return;
 
@@ -92,7 +97,7 @@ void createPageTableEntries(uint32_t physical_adr, uint32_t virtual_adr, uint32_
     uint32_t entries     = end_section - start_section + 1;
 
     for(uint32_t i = 0; i < entries; ++i) {
-        section.bits.base_address = (physical_base + i) & 0x0FFFu;
+        if(!no_access) { section.bits.base_address = (physical_base + i) & 0x0FFFu; }
         // Use loop index to increment virtual section index
         _mmu_level1_table_start_[start_section + i] = section.value;
     }
@@ -119,21 +124,21 @@ void init_mmu_for_current_partition() {
     uint32_t partition_length = partition_end - partition_start;
 
     // Enable RWX for the current partition
-    createPageTableEntries(partition_start, partition_start, partition_length, true, false, false);
+    createPageTableEntries(partition_start, partition_start, partition_length, true, false, false, false);
 
 
     // Map the shared memory between partitions as RW, but not X
     uint32_t shared_base = (uint32_t)&__shared_ctx_base__;
-    createPageTableEntries(shared_base, shared_base, 0x100000, true, false, true); // 1MB pauschal
+    createPageTableEntries(shared_base, shared_base, 0x100000, true, false, true, false); // 1MB
 
 
     // Map the peripherals as RW, non-execute and non-cacheable for IO
-    createPageTableEntries(PERIPHERALS_BASE, PERIPHERALS_BASE, PERIPHERALS_SIZE, false, false, true);
-    createPageTableEntries(ARM_LOCAL_BASE, ARM_LOCAL_BASE, 0x00100000, false, false, true);
+    createPageTableEntries(PERIPHERALS_BASE, PERIPHERALS_BASE, PERIPHERALS_SIZE, false, false, true, false);
+    createPageTableEntries(ARM_LOCAL_BASE, ARM_LOCAL_BASE, 0x00100000, false, false, true, false);
 
 
     // -------------------------------------------------------------------
-    // 7. Enable MMU
+    // Enable MMU
     // -------------------------------------------------------------------
     uintptr_t table_addr = (uintptr_t)_mmu_level1_table_start_;
     // Since table is (4096 * 4 Bytes / 0x4000) large
@@ -158,7 +163,7 @@ void init_mmu_for_current_partition() {
     enable_mmu();
 }
 
-// Only flush specific entries for better0 performance
+// Only flush specific entries for better performance
 static void flush_page_table_entries(uint32_t vaddr, uint32_t length) {
     uint32_t  startIdx = vaddr >> 20;
     uint32_t  endIdx   = (vaddr + length - 1) >> 20;
@@ -179,15 +184,14 @@ static void flush_page_table_entries(uint32_t vaddr, uint32_t length) {
 void partition_switch_mmu_caller() {
 
     uint32_t next_partition_start = partition_base_address;
-    uint32_t num_partitions       = (uint32_t)&__image_count__;
+    uint32_t num_partitions = (uint32_t)&__image_count__;
     uint32_t next_partition_len;
     for(uint32_t i = 0; i < num_partitions; i++) {
         if(partition_base_address == __partition_table_start__[i].start_addr) { next_partition_len = __partition_table_start__[i].length; }
     }
 
-    // uint32_t next_partition_len = partition_lengths[0];
-    //  Set the new partition to RWX
-    createPageTableEntries(next_partition_start, next_partition_start, next_partition_len, true, false, false);
+    // Set the new partition to RWX
+    createPageTableEntries(next_partition_start, next_partition_start, next_partition_len, true, false, false, false);
     flush_page_table_entries(next_partition_start, next_partition_len);
 
 
@@ -200,7 +204,7 @@ void partition_switch_mmu_caller() {
 // Is called in the partition that was switched to
 // Configures the MMU to allow no access to any old partitions anymore
 void partition_switch_mmu_callee() {
-    uint32_t current_index  = (uint32_t)&__image_index__;
+    uint32_t current_index = (uint32_t)&__image_index__;
     uint32_t num_partitions = (uint32_t)&__image_count__;
 
 
@@ -209,9 +213,9 @@ void partition_switch_mmu_callee() {
         if(i == current_index) { continue; }
 
         uint32_t other_start = __partition_table_start__[i].start_addr;
-        uint32_t other_len   = __partition_table_start__[i].length;
+        uint32_t other_len = __partition_table_start__[i].length;
 
-        createPageTableEntries(other_start, other_start, other_len, true, true, false);
+        createPageTableEntries(other_start, other_start, other_len, true, true, false, true);
         flush_page_table_entries(other_start, other_len);
     }
 
